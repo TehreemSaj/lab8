ee75c6161fb63eab8e18218c8190ae3493e39796
zehra <zehra.zaidi2@ucalgary.ca>
2024-03-13 22:40:28 -0600

    took ss

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Zehra Zaidi</td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Tehreem Sajid</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
