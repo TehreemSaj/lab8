62fa8ad5faecd49b5394b16f7ce9a353218f3fca
zehra <zehra.zaidi2@ucalgary.ca>
2024-03-13 23:19:02 -0600

    k

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 // src/components/ProductList.js
-import React, { useEffect, useState} from 'react';
+import React, { useEffect} from 'react';
 import { fetchProducts } from '../services/apiService';
 import { useProductsContext } from '../context/ProductsContext';
 import { useNavigate } from 'react-router-dom';
